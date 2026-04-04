9eff19306f86823d2c9513ca1dad765861aa7784
Bhushan <bhushan.rajput.l1odevs@gmail.com>
2026-04-04 16:32:57 +0530

    Finalising the booking system

--- Day3/BookingSystem.go
+++ Day3/BookingSystem.go
@@ -49,6 +49,8 @@ var mockDestinations = []destination{{
 	destinationCode: "SGD",
 }}
 
+var travelsDatabase []travels = []travels{}
+
 func checkIfEnteredDestinationAvailable(code string) bool {
 	for _, value := range mockDestinations {
 		if strings.EqualFold(code, value.destinationCode) {
@@ -58,12 +60,35 @@ func checkIfEnteredDestinationAvailable(code string) bool {
 	return false
 }
 
+func (p passanger) getUserDetails() {
+	fmt.Println()
+	fmt.Println("Passenger details :")
+	fmt.Printf("Name: %s\nAge: %d\nAddress: %s\n", p.name, p.age, p.address)
+	fmt.Println()
+}
+
+func readUserDetails(p passanger) passanger {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the passanger details: ")
+	fmt.Print("Enter name: ")
+	name, _ := reader.ReadString('\n')
+	p.name = strings.TrimSpace(name)
+	fmt.Print("Enter age: ")
+	str, _ := reader.ReadString('\n')
+	age, _ := strconv.Atoi(strings.TrimSpace(str))
+	p.age = age
+	fmt.Print("Enter address: ")
+	address, _ := reader.ReadString('\n')
+	p.address = strings.TrimSpace(address)
+	return p
+}
+
 func BookingSystem() {
 	fmt.Println("Welcome to leo travels")
 	fmt.Println("Please see below the list of destinations")
 	fmt.Println()
 	for _, value := range mockDestinations {
-		fmt.Printf("Name : %s\nState : %s\nFare : %s\n", value.name, value.continent, value.fare)
+		fmt.Printf("Destination Code:%s\nName : %s\nState : %s\nFare : %s\n", value.destinationCode, value.name, value.continent, value.fare)
 		fmt.Println()
 	}
 	var option string = "No"
@@ -71,24 +96,34 @@ func BookingSystem() {
 	code, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	code = strings.TrimSpace(code)
 	exists := checkIfEnteredDestinationAvailable(code)
+	if exists == false {
+		fmt.Println("Destination not available... ")
+		os.Exit(0)
+	}
 	fmt.Print("To proceed with your booking, choose Y/N : ")
 	fmt.Scanln(&option)
 	if strings.ToLower(option) == "y" && exists {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Please enter the passanger details: ")
-		fmt.Print("Enter name: ")
-		name, _ := reader.ReadString('\n')
-		name = strings.TrimSpace(name)
-		fmt.Print("Enter age: ")
-		str, _ := reader.ReadString('\n')
-		age, _ := strconv.Atoi(strings.TrimSpace(str))
-		fmt.Print("Enter address: ")
-		address, _ := reader.ReadString('\n')
-		address = strings.TrimSpace(address)
-		fmt.Println(age)
+		var p passanger = passanger{}
+		p = readUserDetails(p)
+		p.getUserDetails()
+		fmt.Println("To proceed, please select y/n : ")
+		paymentConfirmation, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if strings.ToLower(paymentConfirmation) == "y" {
+			t := travels{
+				id:                 "1234456",
+				passangerDetails:   p,
+				destinationDetails: mockDestinations[0],
+			}
+			travelsDatabase = append(travelsDatabase, t)
+			fmt.Println(travelsDatabase)
+			os.Exit(0)
+		} else {
+			fmt.Println("No payment method selected...")
+			os.Exit(0)
+		}
 		os.Exit(0)
 	} else {
-		fmt.Println("Destination you entered is not available currently")
+		fmt.Println("Destination you entered is not available currently...")
 		os.Exit(0)
 	}
 
