d78d68a15259fea29229fd08b1e49271be08e3d1
Bhushan <bhushan.rajput.l1odevs@gmail.com>
2026-04-07 12:52:25 +0530

    Added a new request to the API server

--- Day6/main.go
+++ Day6/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Result struct {
+	Name string `json:"name"`
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Backend bootcamp started 🚀")
 }
 
+func bootcampHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	d := Result{Name: "Bhushan"}
+	jData, err := json.Marshal(d)
+	if err != nil {
+		fmt.Println("Failed to encode json", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jData)
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/bootcamp", bootcampHandler)
 
 	fmt.Println("Server running on PORT :8080")
 	http.ListenAndServe(":8080", nil)
